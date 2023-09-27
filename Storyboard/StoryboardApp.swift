dac8a506af200318699ace5e9c74e808638682a9
CafeCat101 <leonore.yardimli@icloud.com>
2023-09-27 17:46:25 +0800

    Initial Commit

---
+++ Storyboard/StoryboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryboardApp.swift
+//  Storyboard
+//
+//  Created by Leonore Yardimli on 2023/9/27.
+//
+
+import SwiftUI
+
+@main
+struct StoryboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
