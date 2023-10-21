39c3089a3870b46e7916985fa6d73095db986aa8
CafeCat101 <leonore.yardimli@icloud.com>
2023-10-21 12:33:21 +0800

    change light mode background

--- Storyboard/Models/PictureModel.swift
+++ Storyboard/Models/PictureModel.swift
@@ -16,84 +16,76 @@ class PictureModel: ObservableObject {
 	var transferableDone = PassthroughSubject<Bool, Never>()
 	var selectedImage: Image?
 	
-	// MARK: - Profile Details
-		
-		@Published var firstName: String = ""
-		@Published var lastName: String = ""
-		@Published var aboutMe: String = ""
-		
-		// MARK: - Profile Image
-		
-		enum ImageState {
-				case empty
+	enum ImageState {
+		case empty
 		case loading(Progress)
 		case success(Image)
 		case failure(Error)
-		}
+	}
+	
+	enum TransferError: Error {
+		case importFailed
+	}
+	
+	struct WordImage: Transferable {
+		let image: Image
 		
-		enum TransferError: Error {
-				case importFailed
-		}
-		
-		struct ProfileImage: Transferable {
-				let image: Image
-				
-				static var transferRepresentation: some TransferRepresentation {
-						DataRepresentation(importedContentType: .image) { data in
-						#if canImport(AppKit)
-								guard let nsImage = NSImage(data: data) else {
-										throw TransferError.importFailed
-								}
-								let image = Image(nsImage: nsImage)
-								return ProfileImage(image: image)
-						#elseif canImport(UIKit)
-								guard let uiImage = UIImage(data: data) else {
-										throw TransferError.importFailed
-								}
-								let image = Image(uiImage: uiImage)
-								return ProfileImage(image: image)
-						#else
-								throw TransferError.importFailed
-						#endif
-						}
+		static var transferRepresentation: some TransferRepresentation {
+			DataRepresentation(importedContentType: .image) { data in
+#if canImport(AppKit)
+				guard let nsImage = NSImage(data: data) else {
+					throw TransferError.importFailed
 				}
-		}
-		
-		@Published private(set) var imageState: ImageState = .empty
-		
-		@Published var imageSelection: PhotosPickerItem? = nil {
-				didSet {
-						if let imageSelection {
-								let progress = loadTransferable(from: imageSelection)
-								imageState = .loading(progress)
-							//self.transferableDone.send(true)
-						} else {
-								imageState = .empty
-						}
+				let image = Image(nsImage: nsImage)
+				return WordImage(image: image)
+#elseif canImport(UIKit)
+				guard let uiImage = UIImage(data: data) else {
+					throw TransferError.importFailed
 				}
+				let image = Image(uiImage: uiImage)
+				return WordImage(image: image)
+#else
+				throw TransferError.importFailed
+#endif
+			}
 		}
-		
+	}
+	
+	@Published private(set) var imageState: ImageState = .empty
+	
+	@Published var imageSelection: PhotosPickerItem? = nil {
+		didSet {
+			if let imageSelection {
+				let progress = loadTransferable(from: imageSelection)
+				imageState = .loading(progress)
+				//self.transferableDone.send(true)
+			} else {
+				imageState = .empty
+			}
+		}
+	}
+	
 	// MARK: - Private Methods
 	
-		private func loadTransferable(from imageSelection: PhotosPickerItem) -> Progress {
-				return imageSelection.loadTransferable(type: ProfileImage.self) { result in
-						DispatchQueue.main.async {
-								guard imageSelection == self.imageSelection else {
-										print("Failed to get the selected item.")
-										return
-								}
-								switch result {
-								case .success(let profileImage?):
-										self.imageState = .success(profileImage.image)
-									self.selectedImage = profileImage.image
-									self.transferableDone.send(true)
-								case .success(nil):
-										self.imageState = .empty
-								case .failure(let error):
-										self.imageState = .failure(error)
-									self.transferableDone.send(true)
-								}
-						}
+	private func loadTransferable(from imageSelection: PhotosPickerItem) -> Progress {
+		return imageSelection.loadTransferable(type: WordImage.self) { result in
+			DispatchQueue.main.async {
+				guard imageSelection == self.imageSelection else {
+					print("Failed to get the selected item.")
+					return
 				}
+				switch result {
+				case .success(let profileImage?):
+					self.imageState = .success(profileImage.image)
+					self.selectedImage = profileImage.image
+					self.transferableDone.send(true)
+				case .success(nil):
+					self.imageState = .empty
+				case .failure(let error):
+					self.imageState = .failure(error)
+					self.transferableDone.send(true)
+				}
+			}
 		}
+	}
 }
